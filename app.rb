82325ccd60ecb39f45190c6689764b6d816e3a18
wesleyfriedman <friedman.wesley@gmail.com>
2016-10-13 17:31:51 -0400

    Done

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
+	get('/hello') do
+		erb :hello
 	end
 
+	get('/goodbye') do
+		erb :goodbye
+	end
+
+	get('/date') do
+		erb :date
+	end
 
 end
